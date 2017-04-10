756cce851b1e4da2374acb02d28a2f950b063c03
kjm21561 <kjm21561@users.noreply.github.com>
2017-04-09 18:27:36 -0700

    Create plot2.r

---
+++ plot2.r
@@ -0,0 +1,24 @@
+## Import Raw data into Rstudio
+getwd()
+data_full <- read.csv(file = "C:/Users/HMF05638/Documents/Coursera2/household_power_consumption.txt",
+                      header = TRUE, sep = ";")
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+
+## Subsetting the imported data
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+## Change Data Format
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+data$Globalactivepowertest <- as.numeric(as.character(data$Global_active_power))
+
+
+## Plot 2
+plot(data$Globalactivepowertest~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+
+## Output diagram
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
